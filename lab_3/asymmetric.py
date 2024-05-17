24057d8903759fba11079ad7bf9708ab976f240a
Sapdotten <semenova.a.p.04@gmail.com>
2024-05-17 16:43:21 +0400

    Add methods to get bytes of keys

--- lab_3/asymmetric.py
+++ lab_3/asymmetric.py
@@ -4,6 +4,7 @@ from typing import Union
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
+from cryptography.hazmat.primitives import serialization
 
 
 class AsymerticEncryption:
@@ -43,6 +44,31 @@ class AsymerticEncryption:
             self.public_key = keys.public_key()
         except Exception as e:
             logging.error(f"Error in generating asymmetric keys: {e}")
+
+    def get_private_key_bytes(self) -> Union[bytes, None]:
+        """
+        Returns bytes of private key
+        :return: bytes, None if error
+        """
+        try:
+            return self.private_key.private_bytes(encoding=serialization.Encoding.PEM,
+                                                  format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                  encryption_algorithm=serialization.NoEncryption())
+        except Exception as e:
+            logging.error(f"Error in try to get bytes of private key: {e}")
+            return None
+
+    def get_public_key_bytes(self) -> Union[bytes, None]:
+        """
+        Returns bytes of public key
+        :return: bytes, None if error
+        """
+        try:
+            return self.public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                                format=serialization.PublicFormat.SubjectPublicKeyInfo)
+        except Exception as e:
+            logging.error(f"Error in try to get bytes of public key: {e}")
+            return None
 
     @staticmethod
     def encrypt_data(key: bytes, data: bytes) -> Union[bytes, None]:
