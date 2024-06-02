c65111f3ea00eda33a8e20e91d911d4edf502e3e
Sapdotten <semenova.a.p.04@gmail.com>
2024-06-03 00:17:31 +0400

    Add exceptions and type hints and descriptions

--- lab_4/tools/file_service.py
+++ lab_4/tools/file_service.py
@@ -1,13 +1,36 @@
 import json
+import logging
+
+from typing import Union
 
 
 class FileService:
+    """
+    Class for working with json files
+    """
     @staticmethod
-    def read_json(path: str) -> dict:
-        with open(path, 'r') as f:
-            return json.load(f)
+    def read_json(path: str) -> Union[dict, None]:
+        '''
+        Reads data from json file
+        :param path: path to file
+        :return: dict with data
+        '''
+        try:
+            with open(path, 'r') as f:
+                return json.load(f)
+        except Exception as e:
+            logging.error(f"Error in try of reading json file: {e}")
+            return None
 
     @staticmethod
     def write_json(data: dict, path: str):
-        with open(path, 'w') as f:
-            json.dump(data, f)
+        """
+        Writes data to json file
+        :param data: data in dict
+        :param path: path to file
+        """
+        try:
+            with open(path, 'w') as f:
+                json.dump(data, f)
+        except Exception as e:
+            logging.error(f"Error in try to write data to json: {e}")
